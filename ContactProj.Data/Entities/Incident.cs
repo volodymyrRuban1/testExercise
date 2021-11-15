0899cab6716724bce166c7e4339b70794c7a64bd
Volodymyr Ruban <Tulen2001@gmail.com>
2021-11-15 18:12:24 +0200

    Added decription to Incident table

--- ContactProj.Data/Entities/Incident.cs
+++ ContactProj.Data/Entities/Incident.cs
@@ -5,6 +5,8 @@ namespace ContactProj.Data.Entities
 	public class Incident
 	{
 		public string Name { get; set; }
+
+		public string Description { get; set; }
 		public ICollection<Account> Account { get; set; }
 	}
 }
